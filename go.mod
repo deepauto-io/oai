4671620aa00675165008ed37555efb9606d21a9f
taoshumin_vendor <tsmidx@gmail.com>
2024-04-17 15:01:20 +0800

    feat: 更新filetype

--- go.mod
+++ go.mod
@@ -7,3 +7,9 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/sashabaranov/go-openai v1.21.0
 )
+
+require (
+	github.com/deepauto-io/filestype v0.0.0-20231217053401-a7e90f2e6b3c
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	golang.org/x/net v0.24.0 // indirect
+)
---
+++ upload.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2024 The oai Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package oai
+
+import (
+	"fmt"
+	"github.com/deepauto-io/filestype"
+	"mime/multipart"
+)
+
+// UploadRequest is the upload request.
+type UploadRequest struct {
+	ConversationId string                `form:"conversation_id"`
+	UploadType     string                `form:"upload_type"`
+	File           *multipart.FileHeader `form:"files"`
+}
+
+func (r *UploadRequest) Validate() error {
+	if r.File == nil {
+		return fmt.Errorf("file is required")
+	}
+
+	if r.UploadType != string(filestype.MyFiles) || r.UploadType != string(filestype.Multimodal) {
+		return fmt.Errorf("invalid upload type")
+	}
+	return nil
+}
